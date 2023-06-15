33170f3fb0260904bf838e210b58b1aeb3c06340
yuvarajmck <136311465+yuvarajmck@users.noreply.github.com>
2023-06-15 19:52:34 +0530

    Delete src/interface_ directory

--- src/interface_/interfaceDemo.java
+++
@@ -1,9 +0,0 @@
-package interface_;
-
-public class interfaceDemo {
-    public static void main(String[] args) {
-        Bike harley = new Bike(10,1, 50);
-        harley.speedUp(1);
-        System.out.println(harley.getSpeed());
-    }
-}
