9ff297474dd523081b32bc3e409c1977f4d45c25
yuvarajmck <136311465+yuvarajmck@users.noreply.github.com>
2023-06-15 19:52:16 +0530

    Delete src/abs/pkg directory

--- src/abs/pkg/AbstractSample.java
+++
@@ -1,9 +0,0 @@
-package abs.pkg;
-
-public  class AbstractSample {
-    public static void main(String[] args) {
-        GraphicObj circle = new Circle();
-        circle.draw();
-        circle.resize();
-    }
-}
