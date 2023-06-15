c1f48f5725f247f43f498d9492f42a22025be524
yuvarajmck <136311465+yuvarajmck@users.noreply.github.com>
2023-06-15 19:52:54 +0530

    Delete src/inheritance/pkg directory

--- src/inheritance/pkg/inheritDemo.java
+++
@@ -1,8 +0,0 @@
-package inheritance.pkg;
-
-public class inheritDemo {
-    public static void main(String[] args) {
-        Bike harley = new Bike(10,1, 50);
-        System.out.println(harley.getHeight());
-    }
-}
